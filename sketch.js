2771291b68da471063b53693ed1cb52b7d82f534
Abhinav2009 <72900427+Abhinav2009@users.noreply.github.com>
2021-01-20 16:49:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(1200, 800);
+  createSprite(400, 200, 50, 50);
+
+
+
+
+
+}
+
+var divisionHeight = 300;
+
+var Particles = [];
+var plinkos = [];
+var divisions = [];
+
+function draw() {
+  background("yellow");  
+  
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  drawSprites();
+}
